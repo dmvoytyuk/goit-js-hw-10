a9a617852251e1c7e6a8aee09f9990e5871402d9
dmvoytyuk <143356320+dmvoytyuk@users.noreply.github.com>
2024-02-06 23:28:58 +0000

    Deploying to gh-pages from @ dmvoytyuk/goit-js-hw-10@139ec6685122faa4337a0c4e332a3ec799522f33 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16229.js";const o=document.querySelector(".form");document.querySelector('input[name="delay"]');const n=document.querySelectorAll('input[name="state"]');console.log(o);function r(e){for(let t=0;t<e.length;t++)e[t].checked}o.addEventListener("submit",e=>{e.preventDefault(),r(n)});
+//# sourceMappingURL=commonHelpers2.js.map
